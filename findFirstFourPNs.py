89366264147a54b6a14e83970148fc6ff0743476
B00145987 Ada Okike <B00145987@mytudublin.ie>
2025-02-17 17:50:53 +0000

    Completed Part C

--- findFirstFourPNs.py
+++ findFirstFourPNs.py
@@ -1,8 +1,17 @@
 #A program here that finds the first four perfect numbers.
 #Ada Okike
-#17/02/2024
+#17/02/2025
 
 #The pseudocode in the README.md file will help you write this program.
+
+tottalPFs = 0
+testNumber = 1
+
+while (totalPFs <= 4):
+    if isPerfectNumber(totalnumber):
+       print(totalNumber, "is a perfect number")
+       totalPFS = totalPFs + 1
+    testNumber = testNumber + 1 
 
 
 
